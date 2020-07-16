10c697bb6a6b9db00678ac005849f0ca411280e9
Herman Jensen <aa427@intility.no>
2020-07-16 12:49:54 +0200

    Update code

--- cra-template/template/src/components/App.tsx
+++ cra-template/template/src/components/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import logo from 'assets/logo.svg';
-import './App.css';
+import React from "react";
+import logo from "assets/logo.svg";
+import { TopBar, Sidebar } from "@intility/bifrost-react";
+import {
+  faHome,
+  faInfoCircle,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+// required for importing Open Sans font and apply it to body
+import "@intility/bifrost-react/dist/bifrost-app.css";
+import "./App.css";
+
+const App = () => (
+  <>
+    <Sidebar>
+      <Sidebar.Item icon={faHome}>Home</Sidebar.Item>
+      <Sidebar.Item icon={faInfoCircle}>About</Sidebar.Item>
+    </Sidebar>
+    <TopBar appName="App Name">
+      <TopBar.Item icon={faUser}>Profile</TopBar.Item>
+    </TopBar>
+    <main>
+      <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,9 +34,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+      </div>
+    </main>
+  </>
+);
 
 export default App;
--- cra-template/template/src/index.tsx
+++ cra-template/template/src/index.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/browser'
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
+import "index.css";
+import App from "components/App";
+import MsalProvider from "@intility/react-msal";
+import * as serviceWorker from "serviceWorker";
 
-if (process.env.environment !== 'development') {
+if (process.env.environment !== "development") {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
     release: process.env.REACT_APP_SENTRY_RELEASE,
-    environment: process.env.REACT_APP_SENTRY_ENVIRONMENT
+    environment: process.env.REACT_APP_SENTRY_ENVIRONMENT,
   });
 }
 
+const msal = {
+  auth: {
+    clientId: "YOUR_CLIENT_ID",
+    // if multi-tenant, use https://login.microsoftonline.com/common
+    authority:
+      "https://login.microsoftonline.com/9b5ff18e-53c0-45a2-8bc2-9c0c8f60b2c6",
+    redirectUri: process.env.REACT_APP_REDIRECT_URI,
+  },
+  endpoints: {
+    "https://graph.microsoft.com": ["User.Read"],
+  },
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider config={msal} forced>
+      <App />
+    </MsalProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
