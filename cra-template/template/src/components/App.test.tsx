c199fb8330738a98cdd984c35f1fec433db03637
Herman Jensen <herman.jensen@intility.no>
2020-10-07 11:13:35 +0200

    Use memory router in tests

--- cra-template/template/src/components/App.test.tsx
+++ cra-template/template/src/components/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { MemoryRouter as Router } from "react-router-dom";
 import { render } from "@testing-library/react";
 import App from "./App";
 
