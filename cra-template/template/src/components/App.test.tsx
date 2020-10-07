4d464f306c370c8b48f300b88e2b9cb8d144f0c7
Herman Jensen <herman.jensen@intility.no>
2020-10-07 11:09:14 +0200

    fix app.test.tsx test

--- cra-template/template/src/components/App.test.tsx
+++ cra-template/template/src/components/App.test.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Router>
+      <App />
+    </Router>
+  );
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
