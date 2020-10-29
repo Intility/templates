c6a221654c4c419c56742066e3a3e62677477fff
Herman Jensen <herman.jensen@intility.no>
2020-10-29 07:32:30 +0100

    fix unused variables

--- cra-template/template/src/components/App.test.tsx
+++ cra-template/template/src/components/App.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  const { getByText } = render(
+  render(
     <Router>
       <App />
     </Router>
