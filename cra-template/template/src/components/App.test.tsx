6caf6219b2bbc9dbfa9b406e6b321b997bff1a3c
Herman Jensen <herman.jensen@intility.no>
2020-10-29 06:26:04 +0100

    update formatting and eslint config

--- cra-template/template/src/components/App.test.tsx
+++ cra-template/template/src/components/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MemoryRouter as Router } from "react-router-dom";
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
