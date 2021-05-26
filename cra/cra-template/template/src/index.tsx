739c42bfabd76eb536f4c2a8e24b64ff9940cb7a
Herman Jensen <herman.jensen@intility.no>
2021-05-26 15:06:24 +0200

    update .gitlab-ci for cra

--- cra/cra-template/template/src/index.tsx
+++ cra/cra-template/template/src/index.tsx
@@ -28,7 +28,7 @@ const msal = {
     // if multi-tenant, use https://login.microsoftonline.com/common
     authority:
       "https://login.microsoftonline.com/9b5ff18e-53c0-45a2-8bc2-9c0c8f60b2c6",
-    redirectUri: process.env.REACT_APP_REDIRECT_URI,
+    redirectUri: window.location.origin,
   },
   endpoints: {
     "https://graph.microsoft.com": ["User.Read"],
