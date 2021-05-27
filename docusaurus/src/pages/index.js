9886cab58f27d04f1c008e7e03325b95c8460590
Herman Jensen <herman.jensen@intility.no>
2021-05-27 13:24:26 +0200

    Clear title and update description on homepage

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
@@ -29,12 +28,8 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout description="Intility templates for Intility developers">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
