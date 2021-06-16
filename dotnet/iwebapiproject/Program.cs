725853acfc1d2bd9bdd21ebb4c4e8f05166d7048
Herman Jensen <herman.jensen@intility.no>
2021-06-16 16:22:42 +0200

    Add Azure App Configuration to Configuration

--- dotnet/iwebapiproject/Program.cs
+++ dotnet/iwebapiproject/Program.cs
@@ -18,6 +18,23 @@ namespace Company.WebApplication1
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddAzureAppConfiguration(builder =>
+                    {
+                        var endpoint = Environment.GetEnvironmentVariable("ASPNETCORE_CONFIGURATION");
+                        var creds = new DefaultAzureCredential(includeInteractiveCredentials: false);
+
+                        // connect to external configuration
+                        builder.Connect(new Uri(endpoint), creds)
+                            // enable keyvault linked items
+                            .ConfigureKeyVault(options => options.SetCredential(creds))
+                            // enable automatic settings refresh
+                            .ConfigureRefresh(options => options
+                                .Register("*", refreshAll: true)
+                                .SetCacheExpiration(TimeSpan.FromSeconds(5)));
+                    }, optional: true);
+                })
                 .UseIntilityLogging((ctx,logging) =>
                 {
                     logging.UseDefaultEnrichers()
