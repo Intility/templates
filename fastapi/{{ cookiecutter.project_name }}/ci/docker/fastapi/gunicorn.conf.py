9ef41df689f31734691614b59534a79c8409a344
Jonas Krüger Svensson <jonas.svensson@intility.no>
2021-10-20 17:01:53 +0200

    fix(fastapi): remove unused entrypoint for dev and minimize gunicorn config, based on feedback from @ingvaldlorentzen

--- fastapi/{{ cookiecutter.project_name }}/ci/docker/fastapi/gunicorn.conf.py
+++ fastapi/{{ cookiecutter.project_name }}/ci/docker/fastapi/gunicorn.conf.py
@@ -1,27 +1,9 @@
 import os
 
-# Only runnable on linux
-cores = len(os.sched_getaffinity(0))  # type: ignore
-
-# Set workers dynamically
-if str_web_concurrency := os.getenv('WEB_CONCURRENCY', None):
-    web_concurrency = int(str_web_concurrency)
-    assert web_concurrency > 0
-else:
-    web_concurrency = max(int(float(os.getenv('WORKERS_PER_CORE', '1')) * cores), 2)
-
-if max_workers := os.getenv('MAX_WORKERS'):
-    web_concurrency = min(web_concurrency, int(max_workers))
-
-# Gunicorn config variables
-workers = web_concurrency
-# bind = os.getenv('BIND', None) or f'{os.getenv("HOST", "0.0.0.0")}:{os.getenv("PORT", "80")}'
-loglevel = os.getenv('LOG_LEVEL', 'info')
-errorlog = os.getenv('ERROR_LOG', None)
-accesslog = os.getenv('ACCESS_LOG', None)
-worker_tmp_dir = '/dev/shm'
+bind = '0.0.0.0:8000'
 keepalive = 5
 max_requests = 1000
 max_request_jitter = 100
-graceful_timeout = 25
-timeout = 30
+worker_class = 'uvicorn.workers.UvicornWorker'
+worker_tmp_dir = '/dev/shm'
+workers = len(os.sched_getaffinity(0))
