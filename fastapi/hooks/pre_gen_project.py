910ec0d8ed3cd2a6239da6a672edabc7ba414687
Jonas Krüger Svensson <jonas.svensson@intility.no>
2021-10-21 15:39:22 +0200

    docs(fastapi): add more prerequisites, add missing git push etc

--- fastapi/hooks/pre_gen_project.py
+++
@@ -1,6 +0,0 @@
-import re
-import sys
-
-if not re.match(r'^[_a-z]+$', '{{ cookiecutter.project_name }}'):
-    print('Invalid characters in project name. Use `_` as a separator and lower case letters')  # noqa
-    sys.exit(1)
