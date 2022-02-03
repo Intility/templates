0670368e3d085dcb9002495b34c3b321da88458e
Thorstein Løkensgard <thorstein.lokensgard@intility.no>
2022-02-03 11:10:30 +0100

    feat(dotnet): add iworker .net template (#100)
    * feat(dotnet): add basic structure for worker service
    
    * feat(dotnet): add Intility logging and Azure App Config
    
    * fix(dotnet): changed package version back after testing
    
    * fix(dotnet): renamed Company.WebApplication1 to Company.Worker1
    
    * fix(dotnet): small tweaks and smoothening edges
    
    Co-authored-by: Herman Jensen <herman.jensen@intility.no>

---
+++ dotnet/iworker/Company.Worker1/Program.cs
@@ -0,0 +1,39 @@
+using Azure.Identity;
+using Company.Worker1;
+using Intility.Extensions.Logging;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(services =>
+    {
+        var configuration = services.Build();
+        var endpoint = configuration["AppConfig"];
+
+        if (!string.IsNullOrEmpty(endpoint))
+        {
+            services.AddAzureAppConfiguration(builder =>
+            {
+                var creds = new DefaultAzureCredential(includeInteractiveCredentials: false);
+                // connect to external configuration
+                builder.Connect(new Uri(endpoint), creds)
+                    // enable keyvault linked items
+                    .ConfigureKeyVault(options => options.SetCredential(creds))
+                    // enable automatic settings refresh
+                    .ConfigureRefresh(options => options
+                        .Register("*", refreshAll: true)
+                        .SetCacheExpiration(TimeSpan.FromSeconds(5)));
+            }, optional: true);
+        }
+    })
+    .UseIntilityLogging((ctx, logging) =>
+    {
+        logging.UseDefaultEnrichers()
+            .UseElasticsearch()
+            .UseSentry();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
