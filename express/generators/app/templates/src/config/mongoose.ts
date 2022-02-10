915486e1b2c93361e276acfbeb57f52706e4ce27
MiniMarker <cfmarker@gmail.com>
2022-02-10 21:17:53 +0100

    fix(express): update mongoose config due to dependency update

--- express/generators/app/templates/src/config/mongoose.ts
+++ express/generators/app/templates/src/config/mongoose.ts
@@ -1,21 +1,17 @@
-import mongoose, { ConnectionOptions } from 'mongoose';
+import mongoose, { ConnectOptions, Mongoose } from 'mongoose';
 import LoggerService from './LoggerService';
 
 const { MONGO_DB_CONNECTION_STRING } = process.env;
 export const connectionString = MONGO_DB_CONNECTION_STRING || 'mongodb://root:example@127.0.0.1:27017/local?authSource=admin';
 
-export const mongooseConfig: ConnectionOptions = {
-    // List of connection config options: https://mongoosejs.com/docs/connections.html#options
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-};
+// List of connection config options: https://mongoosejs.com/docs/connections.html#options
+export const mongooseConfig: ConnectOptions = { };
 
 
 const logger = new LoggerService('Mongoose');
 
 // OPTIONAL: Connection events
-// You can tap into mongoose internal event system and execute custom logic when given connection events accures
+// You can tap into mongoose internal event system and execute custom logic when given connection events occurs
 mongoose.connection.on('connecting', () => {
     logger.info(`Connecting to Database`);
 });
@@ -32,7 +28,7 @@ mongoose.connection.on('error', (err) => {
     logger.error(`Database connection failed`, err);
 });
 
-export const connect = (): Promise<typeof mongoose> => mongoose.connect(connectionString, mongooseConfig);
+export const connect = (): Promise<Mongoose> => mongoose.connect(connectionString, mongooseConfig);
 
 // export the customized mongoose instance
 export default mongoose;
