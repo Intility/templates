676be6333971b169927c18f13395518f8914a052
Jonas Krüger Svensson <jonas.svensson@intility.no>
2022-06-10 16:09:05 +0200

    fix(fastapi): remove unused variables, add example.env file and sort settings on alphabet

--- fastapi/README.md
+++ fastapi/README.md
@@ -8,12 +8,18 @@ This is the home for Intility FastAPI templates.
 * [`pre_gen_project.py`](hooks/pre_gen_project.py) runs before cookiecutter is run.
 * [`post_gen_project.py`](hooks/post_gen_project.py) runs after cookiecutter is run. This will remove files
 based on selections.
-  * Multiple files are completely deleted based on user selections!
-
 
 ## Usage
-* `poetry init` from `templates` folder.
-* `poetry add cruft`
-* `cruft create . --directory="fastapi"` to use from source (current commit)
-  * `cookiecutter fastapi` to use from current state (used while testing uncommitted changes)
-* `cruft create https://github.com/Intility/templates.git --directory="fastapi"` to use from GitHub
+In order to use the latest _released_ template, please follow the documentation over at
+[create.intility.app](https://create.intility.app/fastapi). 
+
+## Editing the template
+1. Create a venv from the `fastapi` folder
+```bash
+poetry update
+```
+2. Make your changes
+3. Use `cookiecutter` to test your local version. 
+```bash
+cookiecutter fastapi
+```
--- fastapi/{{ cookiecutter.project_name }}/app/core/config.py
+++ fastapi/{{ cookiecutter.project_name }}/app/core/config.py
@@ -20,19 +20,17 @@ env = Env()
 
 
 class Credentials(CustomBaseSettings):
-    {% if cookiecutter.sqlmodel == 'True' %}
-    POSTGRES_USER: str = Field('{{ cookiecutter.project_name }}', env='POSTGRES_USER')
-    POSTGRES_PASSWORD: str = Field('', env='POSTGRES_PASSWORD')
-    POSTGRES_URL: str = Field('localhost', env='POSTGRES_URL')
-    {% endif %}
-    {% if cookiecutter.authentication_strategy == 'FastAPI Azure Auth (default)' %}
-    TENANT_ID: str = Field(default='9b5ff18e-53c0-45a2-8bc2-9c0c8f60b2c6', description='Intility tenant ID')
+    {% if cookiecutter.authentication_strategy == 'FastAPI Azure Auth (default)' %} # Azure
     APP_CLIENT_ID: str = Field(..., description='ClientID for web-app. (First step)')
     OPENAPI_CLIENT_ID: str = Field(..., description='OpenAPI/Swagger SPA client ID')
+    TENANT_ID: str = Field(default='9b5ff18e-53c0-45a2-8bc2-9c0c8f60b2c6', description='Intility tenant ID')
     {% endif %}
-    {% if cookiecutter.authentication_strategy != 'FastAPI Azure Auth (default)' and cookiecutter.sqlmodel == 'False' %}
-    ...
-    {% endif %}
+    {% if cookiecutter.sqlmodel == 'True' %} # Database
+    POSTGRES_PASSWORD: str = Field('', env='POSTGRES_PASSWORD')
+    POSTGRES_USER: str = Field('{{ cookiecutter.project_name }}', env='POSTGRES_USER')
+    POSTGRES_URL: str = Field('localhost', env='POSTGRES_URL')
+    {% endif %}{% if cookiecutter.authentication_strategy != 'FastAPI Azure Auth (default)' and cookiecutter.sqlmodel == 'False' %}
+    ...{% endif %}
 
 
 credentials = Credentials()
@@ -44,12 +42,11 @@ class SettingsWithDerivedValues(CustomBaseSettings):
     Field-class has determined a fields actual value (default or based from an environment variable).
     This doesn't work if defined in the same settings class.
     """
-    {% if cookiecutter.sqlmodel == 'True' %}
+    {% if cookiecutter.sqlmodel == 'True' %}# Database
     POSTGRES_CONNECTION_STRING: str = Field(
         f'postgresql+asyncpg://{credentials.POSTGRES_USER}:{credentials.POSTGRES_PASSWORD}@{credentials.POSTGRES_URL}',
-    )
-    {% endif %}
-    {% if cookiecutter.authentication_strategy == 'FastAPI Azure Auth (default)' %}
+    ){% endif %}
+    {% if cookiecutter.authentication_strategy == 'FastAPI Azure Auth (default)' %}# Azure
     SCOPES: dict[str, str] = Field(
         {
             f'api://{credentials.APP_CLIENT_ID}/user_impersonation': 'User impersonation',
@@ -58,11 +55,8 @@ class SettingsWithDerivedValues(CustomBaseSettings):
             'A dictionary with scopes, used for OpenAPI authentication/authorization. '
             'You can add multiple scopes here.'
         ),
-    )
-    {% endif %}
-    {% if cookiecutter.authentication_strategy != 'FastAPI Azure Auth (default)' and cookiecutter.sqlmodel == 'False' %}
-    ...
-    {% endif %}
+    ){% endif %}
+    {% if cookiecutter.authentication_strategy != 'FastAPI Azure Auth (default)' and cookiecutter.sqlmodel == 'False' %}...{% endif %}
 
 
 class Settings(
@@ -72,13 +66,13 @@ class Settings(
 ):
     API_V1_STR: str = '/api/v1'
     PROJECT_NAME: str = '{{ cookiecutter.project_name }}'
-    SECRET_KEY: str = Field(..., env='SECRET_KEY')
     SENTRY_DSN: str = Field('', env='SENTRY_DSN')
-
+    {% if cookiecutter.authentication_strategy == 'FastAPI Azure Auth (default)' %}
     # CORS settings should answer this question: Which frontends (through browser) are allowed to access the API?
     # This includes your OpenAPI documentation, your react frontend etc.
     BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = ['http://localhost:8000']
     BACKEND_CORS_ORIGINS_REGEX: str = r'https://.*\.intility\.(no|com)'
+    {% endif %}
 
 
 settings = Settings()
