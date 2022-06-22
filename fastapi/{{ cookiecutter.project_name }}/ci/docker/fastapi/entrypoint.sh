d219ec4f7028de6bb4113ff8446c0695d26d36a8
Jonas Krüger Svensson <jonas.svensson@intility.no>
2022-06-22 15:17:43 +0200

    fix(fastapi): fix wrong project name in dockerfile and wrong python version in config file

--- fastapi/{{ cookiecutter.project_name }}/ci/docker/fastapi/entrypoint.sh
+++ fastapi/{{ cookiecutter.project_name }}/ci/docker/fastapi/entrypoint.sh
@@ -2,17 +2,9 @@
 
 {% if cookiecutter.sqlmodel == 'True' %}
 # -- Perform database migrations
-run_migrations() {
-  echo "Running migrations"
-  alembic upgrade head
-}
+echo "Running migrations"
+alembic upgrade head
 {% endif %}
 
-run_web_server() {
-  echo "Starting web server"
-  exec gunicorn app.main:app --worker-class uvicorn.workers.UvicornWorker --config ci/docker/fastapi/gunicorn.conf.py
-}
-{% if cookiecutter.sqlmodel == 'True' %}
-run_migrations
-{% endif %}
-run_web_server
+echo "Starting web server"
+exec gunicorn app.main:app --config ci/docker/fastapi/gunicorn.conf.py
