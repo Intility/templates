dd2183db13f908229e341fa39968a3ac649b21e3
Christian Marker / Intility AS <christian.marker@intility.no>
2022-08-24 16:01:31 +0200

    feat(nest): add configuration for ConfigModule
    #178

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvironmentVariables } from './interfaces/environment-variables.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	const configService = app.get<ConfigService<EnvironmentVariables, true>>(ConfigService);
+	const port = configService.get('PORT', { infer: true });
+
+  	await app.listen(port || 4000);
 }
 bootstrap();
