76e2429cf9fe1941ea104a8e8d51b131286d28a1
Christian Marker / Intility AS <christian.marker@intility.no>
2022-08-25 13:54:55 +0200

    feat(nest): add swagger decorators to dtos and controller

--- nest/src/issue/dto/create-issue.dto.ts
+++ nest/src/issue/dto/create-issue.dto.ts
@@ -1,4 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateIssueDto {
-    title: string;
+	@ApiProperty({
+		description: 'Title of the issue',
+		example: 'Implement NestJs template',
+		type: String,
+		required: true
+	})
+	title: string;
+	
+	@ApiProperty({
+		description: 'Descriptive text regarding the issue',
+		example: 'Lorem ipsum dolor sit amet',
+		type: String,
+		required: false
+	})
     description: string | null;
 }
--- nest/src/issue/dto/update-issue.dto.ts
+++ nest/src/issue/dto/update-issue.dto.ts
@@ -1,6 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateIssueDto } from './create-issue.dto';
 
 export class UpdateIssueDto extends PartialType(CreateIssueDto) {
+	@ApiProperty({
+		type: Boolean,
+		example: true,
+		description: 'Is the issue open, or closed?',
+		required: false
+	})
     open?: boolean;
 }
