7acec1da696e4f2ca006382fce103a8804d54e52
Christian Marker / Intility AS <christian.marker@intility.no>
2022-08-25 14:18:30 +0200

    feat(nest): add comments explaining swagger configurations

--- nest/src/config/swagger.ts
+++ nest/src/config/swagger.ts
@@ -16,11 +16,14 @@ export const configureSwagger = (app: INestApplication): void => {
 	const buildVersion = configService.get('BUILD_VERSION', { infer: true });
 
 	const microsoftLoginBaseUrl = `https://login.microsoftonline.com/${azureTenantId}/oauth2/v2.0`;
+	const scope = `api://${appClientId}/User.Impersonation`
 
 	const swaggerConfig = new DocumentBuilder()
         .setTitle(name)
         .setDescription(description)
-        .setVersion(buildVersion || '')
+		.setVersion(buildVersion || '')
+		// Configure Swagger Authentication
+		// Official Swagger Documentation: https://swagger.io/docs/specification/authentication/oauth2/
         .addOAuth2({
             type: 'oauth2',
             name: AdminOAuthFlow,
@@ -29,7 +32,7 @@ export const configureSwagger = (app: INestApplication): void => {
                     authorizationUrl: `${microsoftLoginBaseUrl}/authorize`,
 					tokenUrl: `${microsoftLoginBaseUrl}/token`,
                     scopes: {
-						[`api://${appClientId}/User.Impersonation`]: 'Access API as User',
+						[scope]: 'Access API as User',
 					},
                 },
             },
@@ -39,13 +42,27 @@ export const configureSwagger = (app: INestApplication): void => {
 	const document = SwaggerModule.createDocument(app, swaggerConfig);
 	
 	SwaggerModule.setup(`api/docs`, app, document, {
-        customSiteTitle: `${name} - Swagger`,
-        swaggerOptions: {
-            persistAuthorization: false,
-            deepLinking: true,
-            oauth: {
+		customSiteTitle: `${name} - Swagger`,
+		// Customize Swagger Client
+		// Official Swagger Documentation: https://swagger.io/docs/open-source-tools/swagger-ui/usage/configuration/
+		swaggerOptions: {
+			// Persists authorization data and it would not be lost on browser close/refresh.
+			// Can be troublesome if you have multiple apis behind the same gateway/base url
+			persistAuthorization: false,
+			// NOTE: If API grows too big, it can be helpful to set this to `none` to disable automatic expansion of all tags
+			docExpansion: 'list',
+			deepLinking: true,
+			// Customize Swagger OAuth configuration
+			// Official Swagger Documentation: https://swagger.io/docs/open-source-tools/swagger-ui/usage/oauth2/
+			oauth: {
+				//  Auto fill API Client ID
                 clientId: swaggerAppClientId,
-                usePkceWithAuthorizationCodeGrant: true,
+				//  Auto select API scope
+				scopes: [scope],
+				// Set custom app name
+				appName: name,
+				// Set OAuth to use OAuth 2.0 authorization code flow with PKCE.
+				usePkceWithAuthorizationCodeGrant: true,
             },
         },
 	});
