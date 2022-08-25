2ea2346a3e708048bf04e33ea762dbf4c2346cbd
Christian Marker / Intility AS <christian.marker@intility.no>
2022-08-25 09:14:50 +0200

    feat(nest): add support for api versioning in server config

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -9,6 +9,9 @@ async function bootstrap() {
 	const configService = app.get<ConfigService<EnvironmentVariables, true>>(ConfigService);
 	const port = configService.get('PORT', { infer: true });
 
+	// Configure API versioning.
+	// Official Nest Documentation: https://docs.nestjs.com/techniques/versioning
+	app.enableVersioning();
   	await app.listen(port || 4000);
 }
 bootstrap();
