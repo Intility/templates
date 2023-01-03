9177b4045ae1e73632c185fca8a685d1838c5883
Herman Jensen <herman.jensen@intility.no>
2023-01-03 14:39:54 +0100

    fix(react): add vitest

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,7 +1,8 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import checker from "vite-plugin-checker";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -21,5 +22,12 @@ export default defineConfig({
   server: {
     open: "http://localhost:3000",
     port: 3000,
+  },
+  // https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts
+  // https://vitest.dev/config/
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   },
 });
