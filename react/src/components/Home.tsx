fd1e6ce92df8dd45ae16c1d4609d67cf354abfee
Herman Jensen <herman.jensen@intility.no>
2023-01-03 09:56:57 +0100

    fix(react): use correct filename on entry page

--- react/src/components/Home.tsx
+++ react/src/components/Home.tsx
@@ -16,7 +16,7 @@ const Home = () => {
       <h1>Intility + Vite + React</h1>
       <div className="card">
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/components/Home.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
